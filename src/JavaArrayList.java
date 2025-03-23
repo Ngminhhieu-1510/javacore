dae992a704f1314fb31ae4138f76a7f96e36927f
Nguyễn Hiếu <nguyenhieu@MacBook-Pro-cua-Nguyen-2.local>
2025-03-23 13:12:37 +0700

    Rename Main.java to JavaArrayList.java

--- src/JavaArrayList.java
+++ src/JavaArrayList.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 
-public class Main {
+public class JavaArrayList {
     public static void main(String[] args) {
         ArrayList<String> team = new ArrayList<>();
         team.add("MU");
         team.add("Liver");
         team.add("Ars");
-        team.removeFirst();
+        team.remove(0);
         System.out.println("top 3: " + team);
     }
 
